70dc32325513d0a3722de41813b7af3327dff528
kalilover526 <rahulk.0526@gmail.com>
2018-06-07 10:12:00 +0530

    first practice set

---
+++ first/prctcset/thirteen.js
@@ -0,0 +1,12 @@
+// var points=[40,100,1,5,25,10]
+// console.log(points)
+// for(let i=0;i<points.length;i++)
+// {
+//     let min=i;
+//     for(let j=i+1;i<=points.length;j++)
+//     {
+//         if(points[j]<points[min]){
+//         min=j;}
+//     }
+
+// }
